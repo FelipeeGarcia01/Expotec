03f4f164c4d351a66ea098b3449e7b7bc29fa6a8
FelipeDesigner <felipegarciameira@gmail.com>
2026-07-03 08:06:05 -0300

    js cor botao

--- Pasta-tela-inicial/interatividade.js
+++ Pasta-tela-inicial/interatividade.js
@@ -3,3 +3,11 @@ const botao_chamada = document.getElementsByClassName("chamada-button")
 botao_chamada.addEventListener("click", function() {
     window.location.href = "Sobre.html"
 } )
+
+botao_chamada.addEventListener("mouseover", function() {
+    botao_chamada.style.backgroundColor = rgb(25, 0, 255)
+})
+
+botao_chamada.addEventListener("mouseout", function() {
+    botao_chamada.style.backgroundColor = rgb(143, 210, 255)
+})
